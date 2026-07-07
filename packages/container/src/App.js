035d64ddec233c6d2e0262c7e3554bb4c98f329d
Hassan <cm.hassan98@gmail.com>
2026-07-07 13:06:36 +0500

    fixed the styling class name issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,14 +3,21 @@ import MarketingApp from "./components/MyApp";
 import { BrowserRouter, Route } from "react-router-dom/";
 import Header from "./components/Header";
 import { Router } from "react-router-dom/cjs/react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <div>
-      {/* <BrowserRouter> */}
-      <Header />
-      <MarketingApp />
-      {/* </BrowserRouter> */}
+      <StylesProvider generateClassName={generateClassName}>
+        {/* <BrowserRouter> */}
+        <Header />
+        <MarketingApp />
+        {/* </BrowserRouter> */}
+      </StylesProvider>
     </div>
   );
 };
